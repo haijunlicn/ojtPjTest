94f7660960d012c2763c583d69c1bd095da0354b
Maykabyarr <maykabyarnaing2004@gmail.com>
2025-05-28 10:29:22 +0630

    may_git

---
+++ GoogleClassroomAngular/src/app/maygit/maygit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maygit',
+  standalone: false,
+  templateUrl: './maygit.component.html',
+  styleUrl: './maygit.component.css'
+})
+export class MaygitComponent {
+
+}
